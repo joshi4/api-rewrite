0061312845aaa81a1484a187aedcfb1c28ebfc59
Shantanu <shantanu@render.com>
2021-03-12 14:47:39 -0800

    add API_HOST

--- server/main.go
+++ server/main.go
@@ -8,10 +8,10 @@ import (
 
 func main() {
 	branch := os.Getenv("RENDER_GIT_BRANCH")
-	ex := os.Getenv("RENDER_EXTERNAL_URL")
+	ex := os.Getenv("API_HOST")
 	http.ListenAndServe(":3000", http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(fmt.Sprintf("external_url is: %s", ex)))
+		w.Write([]byte(fmt.Sprintf("API_HOST is: %s", ex)))
 		if branch == "" || branch == "master" || branch == "main" {
 			w.Write([]byte("main branch\n"))
 			return
