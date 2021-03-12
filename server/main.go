7cacff299b7c97cdfe5d05153e3d4d29e73234fe
Shantanu <shantanu@render.com>
2021-03-12 14:38:37 -0800

    repro

--- server/main.go
+++ server/main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 )
 
 func main() {
 	branch := os.Getenv("RENDER_GIT_BRANCH")
+	ex := os.Getenv("RENDER_EXTERNAL_URL")
 	http.ListenAndServe(":3000", http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(fmt.Sprintf("external_url is: %s", ex)))
 		if branch == "" || branch == "master" || branch == "main" {
-			w.Write([]byte("main branch"))
+			w.Write([]byte("main branch\n"))
 			return
 		}
 		w.Write([]byte("feature branch"))
