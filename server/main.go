1a15b834cda95169fbc6543a04c9ea6ad2b65fd2
Shantanu <shantanu@render.com>
2022-03-29 14:09:17 -0700

    line

--- server/main.go
+++ server/main.go
@@ -7,6 +7,7 @@ import (
 )
 
 func main() {
+	fmt.Println("starting...")
 	branch := os.Getenv("RENDER_GIT_BRANCH")
 	ex := os.Getenv("API_HOST")
 	http.ListenAndServe(":3000", http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
