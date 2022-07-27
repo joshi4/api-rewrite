7fb907dc41143a009cf0db83504fdfdfbf6e0d9f
Shantanu <joshishantanu4@gmail.com>
2022-07-27 00:30:20 -0700

    try now

--- server/cmd/foo/main.go
+++ server/cmd/foo/main.go
@@ -1,6 +1,25 @@
 package main
 
+import "reflect"
+
 // program to print hello world
 func main() {
-	println("Hello World")
+	println(nillifyZeroValueBuildFilter(nil) == nil)
+	println(nillifyZeroValueBuildFilter(&BuildFilter{}) == nil)
+}
+
+type BuildFilter struct {
+	Paths        []string
+	IgnoredPaths []string
+}
+
+func nillifyZeroValueBuildFilter(filter *BuildFilter) *BuildFilter {
+	if filter == nil {
+		return nil
+	}
+	if reflect.DeepEqual(*filter, BuildFilter{}) {
+		return nil
+	}
+	return filter
+	// side note: maybe we can rewrite this func while using filter.IsEmpty()
 }
