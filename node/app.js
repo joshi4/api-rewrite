03fe5b41d86d4cb18672e614a79a531b846f0f7f
Shantanu <joshishantanu4@gmail.com>
2023-01-26 14:04:08 -0800

    node check

--- node/app.js
+++ node/app.js
@@ -3,7 +3,6 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send(html));
-
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
