2541c03dc5db6aa4b966fec834e7aead0a2e9277
Shantanu <joshishantanu4@gmail.com>
2023-01-26 14:51:50 -0800

    go change

--- server/main.go
+++ server/main.go
@@ -9,7 +9,6 @@ import (
 )
 
 func main() {
-
 	branch := os.Getenv("RENDER_GIT_BRANCH")
 	ex := os.Getenv("API_HOST")
 	http.ListenAndServe(":3000", http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
